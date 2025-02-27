4be4bd52fc13d757d6f5b63d9623e5b30348edcb
henrique <henriqueacciarito0@gmail.com>
2025-02-26 22:34:02 -0300

    vlw tmj

---
+++ var idade=18.js
@@ -0,0 +1,13 @@
+var idade=18
+if(idade>=18){
+    console.log("voto obrigatorio");}
+    var idade=16
+    if(idade<=18){
+        console.log("voto opcional");}
+        var idade=71
+        if(idade>=70){
+            console.log("voto opcional")}
+            var idade=15
+            if(idade<=16){
+                console.log("nao pode votar")}
+
